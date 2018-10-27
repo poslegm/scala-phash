82e84222b36a04a41ca7876248bb3256a1d72731
Mikhail Chugunkov <poslegm@gmail.com>
2018-10-27 21:53:53 +0300

    Extended MathUtils

--- src/main/scala/com/github/poslegm/scalaphash/MathUtils.scala
+++ src/main/scala/com/github/poslegm/scalaphash/MathUtils.scala
@@ -3,7 +3,7 @@ package com.github.poslegm.scalaphash
 private[scalaphash] object MathUtils {
   type FloatMatrix = Array[Array[Float]]
 
-  implicit class FloatMatrixOps(matrix: FloatMatrix) {
+  implicit class FloatMatrixOps(val matrix: FloatMatrix) extends AnyVal {
     def *(other: FloatMatrix): FloatMatrix = {
       if (matrix.length == 0 || matrix(0).length == 0 || matrix(0).length != other.length) {
         throw new IllegalArgumentException(s"Can't multiply matrices")
@@ -19,5 +19,9 @@ private[scalaphash] object MathUtils {
       * */
     def crop(x1: Int, y1: Int, x2: Int, y2: Int): FloatMatrix =
       Array.tabulate(x2 - x1 + 1, y2 - y1 + 1)((i, j) => matrix(i + x1)(j + y1))
+  }
+
+  implicit class FloatOps(val x: Float) extends AnyVal {
+    def ~=(y: Float, delta: Float = 0.000001f): Boolean = y - delta <= x && x <= y + delta
   }
 }
